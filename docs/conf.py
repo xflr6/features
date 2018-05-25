72ef38977215e2e3fcf7681d1ebdfa620d307814
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2018-05-25 19:20:42 +0200

    release 0.5.9

--- docs/conf.py
+++ docs/conf.py
@@ -61,7 +61,7 @@ author = u'Sebastian Bank'
 # built documents.
 #
 # The short X.Y version.
-version = u'0.5.9.dev0'
+version = u'0.5.9'
 # The full version, including alpha/beta/rc tags.
 release = version
 
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='features',
-    version='0.5.9.dev0',
+    version='0.5.9',
     author='Sebastian Bank',
     author_email='sebastian.bank@uni-leipzig.de',
     description='Feature set algebra for linguistics',
