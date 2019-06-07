9d7cafda377a4630831135d1fcef2921bb89bb19
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2019-06-07 10:12:42 +0200

    bump version for development

--- features/__init__.py
+++ features/__init__.py
@@ -8,7 +8,7 @@ from .systems import FeatureSystem
 __all__ = ['Config', 'FeatureSystem', 'add_config', 'make_features']
 
 __title__ = 'features'
-__version__ = '0.5.11'
+__version__ = '0.5.12.dev0'
 __author__ = 'Sebastian Bank <sebastian.bank@uni-leipzig.de>'
 __license__ = 'MIT, see LICENSE.txt'
 __copyright__ = 'Copyright (c) 2014-2017 Sebastian Bank'
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='features',
-    version='0.5.11',
+    version='0.5.12.dev0',
     author='Sebastian Bank',
     author_email='sebastian.bank@uni-leipzig.de',
     description='Feature set algebra for linguistics',
