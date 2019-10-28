5801cdb7d78f22f4b75b86c5bc48db05334f3f4a
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2019-10-28 02:20:15 +0100

    tag Python 3.8 support

--- setup.py
+++ setup.py
@@ -40,6 +40,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Scientific/Engineering :: Artificial Intelligence',
         'Topic :: Scientific/Engineering :: Information Analysis',
         'Topic :: Scientific/Engineering :: Visualization',
