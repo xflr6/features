d8f7ee0c7381fc5abc71aa7047f32af7b828ebf3
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2021-12-04 11:20:36 +0100

    drop name duplication

--- visualize-systems.py
+++ visualize-systems.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-# visualize-systems.py
 
 import features.visualize
 
 DIRECTORY = 'visualize-output'
 
 FORMAT = 'pdf'
+
 
 features.visualize.render_all(directory=DIRECTORY, format=FORMAT)
